61df8b0f9f98e5428455ac918a695c66097229e8
Daan <daandrenthenpr@gmail.com>
2024-07-03 20:54:21 +0200

    Portfolio - Homepage added

---
+++ Porfolio - Homepage/views/footer.blade.php
@@ -0,0 +1,9 @@
+<div class="container-footer pt-4 mt-5 pb-4">
+    <div class="row">
+        <div class="col"></div>
+        <div class="col"></div>
+        <div class="col"></div>
+        <div class="col"></div>
+    </div>
+</div>
+
---
+++ Porfolio - Homepage/views/header.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+        <b><div class="container-navbar fixed-top">
+            <div class="row d-flex align-items-center justify-content-center p-3 heebo fs20">
+                <a class="navbar-brand mr-5" href="/"><img src="{{url('content/img/logoBlack.png')}}" alt="logo"></a>
+                <div class="nav-item ml-5 d-none d-lg-block"><a class="nav-link" href="/"><img src="{{url('content/icons/homeIcon.png')}}" alt="homeIcon"> Home</a></div>
+                <div class="nav-item d-none d-lg-block"><a class="nav-link" href="/ervaring"><img src="{{url('content/icons/ervaringIcon.png')}}" alt="ervaringIcon"> Ervaring</a></div>
+                <div class="nav-item d-none d-lg-block"><a class="nav-link" href="/projecten"><img src="{{url('content/icons/projectenIcon.png')}}" alt="projectenIcon"> Projecten</a></div>
+                <button class="contactButton">Contact<div class="iconButton">   <svgheight="24"  width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg></div></button>
+            </div>
+        </div>
+        </b>
+</body>
+</html>
