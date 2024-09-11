d61c3b1a4af36e34953b82e0a94dd3d154efbbee
Daan <daandrenthenpr@gmail.com>
2024-09-11 10:13:08 +0200

    Update Travel.php

--- TravelTrack Files/Models/Travel.php
+++ TravelTrack Files/Models/Travel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Travel extends Model // Verander de klasse naam naar Travel
+class Travel extends Model 
 {
     use HasFactory;
 
