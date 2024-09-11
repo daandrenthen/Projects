172c15a1215e02d94c754604dbf11ffe5a633a83
Daan <daandrenthenpr@gmail.com>
2024-09-11 11:12:58 +0200

    Update MailController.php

--- Mail function PHP/MailController.php
+++ Mail function PHP/MailController.php
@@ -12,20 +12,19 @@ class MailController extends Controller
     public function sendMail(Request $request)
     {
         $mailData = [
-            'title' => $request->first_name . ' ' . $request->last_name, // Gebruik de opgegeven naam als titel
+            'title' => $request->first_name . ' ' . $request->last_name, 
             'first_name' => $request->first_name,
             'last_name' => $request->last_name,
             'email' => $request->email,
             'body' => $request->message
         ];
 
-        // Set the sender's email and name dynamically
+   
         config(['mail.from.address' => $request->email]);
         config(['mail.from.name' => $request->first_name . ' ' . $request->last_name]);
 
         Mail::to('daandrenthenpr@gmail.com')->send(new DemoMail($mailData));
 
-        // Restore the default mail from address and name
         config(['mail.from.address' => env('MAIL_FROM_ADDRESS')]);
         config(['mail.from.name' => env('MAIL_FROM_NAME')]);
 
